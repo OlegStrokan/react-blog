84284c3d7290ffa757465aedf2dd3eeb35b87fd6
Oleh Strokan <oleg14ua71@gmail.com>
2023-07-24 08:50:59 +0200

    v3. env

--- config/build/build-webpack-config.ts
+++ config/build/build-webpack-config.ts
@@ -1,14 +1,14 @@
-import path from "path";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import { buildLoaders } from "./build.loaders";
 import { buildPlugins } from "./build.plugins";
 import { buildResolvers } from "./build.resolvers";
+import { buildDevServer } from "./build-dev-server";
 
 export const buildWebpackConfig = (
   options: BuildOptions
 ): webpack.Configuration => {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
   return {
     mode: mode,
     entry: paths.entry,
@@ -22,5 +22,7 @@ export const buildWebpackConfig = (
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 };
