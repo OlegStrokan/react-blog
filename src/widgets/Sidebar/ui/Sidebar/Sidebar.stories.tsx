40029eb6936a9ceaf1dc1abef84b1ea9829e4299
Oleh Strokan <oleg14ua71@gmail.com>
2023-08-10 16:33:20 +0200

    v27. fix dev plugins

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import AboutPage from './AboutPage';
+import { Sidebar } from './Sidebar';
 
 export default {
-    title: 'pages/AboutPage',
-    component: AboutPage,
+    title: 'widget/Sidebar',
+    component: Sidebar,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} as ComponentMeta<typeof AboutPage>;
+} as ComponentMeta<typeof Sidebar>;
 
-const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args} />;
+const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 
-export const Normal = Template.bind({});
-Normal.args = {};
+export const Light = Template.bind({});
+Light.args = {};
 
 export const Dark = Template.bind({});
 Dark.args = {};
